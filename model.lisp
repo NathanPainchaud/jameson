554f68bc85d93e1eb591d0ee37b063d5521400a8
NolwennBernard <bernard_nolwenn@yahoo.fr>
2018-11-26 09:33:19 -0500

    Retrieval production - not tested

--- model.lisp
+++ model.lisp
@@ -15,11 +15,12 @@
 (chunk-type position position-x position-y)
 (chunk-type trajectory position1 position2 position3 course)
 (chunk-type projectile id trajectory)
+(chunk-type estimation dx dy y action)
 (chunk-type goal state tracked pos-x pos-y)
 
 (add-dm
   (goal isa goal)
-  (attending)(attended))
+  (attending)(attended)(retrieving)(done))
 
 
 (P respond
@@ -42,8 +43,8 @@
       ISA         goal
       state       nil
    =visual-location>
-   		screen-x				=pos-x
-   		screen-y				=pos-y
+         screen-x          =pos-x
+         screen-y          =pos-y
    ?visual>
       state       free
 ==>
@@ -52,8 +53,8 @@
       screen-pos  =visual-location
    =goal>
       state       attended
-      pos-x				=pos-x
-      pos-y 			=pos-y
+      pos-x          =pos-x
+      pos-y          =pos-y
 
 )
 
@@ -65,10 +66,80 @@
       value       =letter
 ==>
    =goal>
-      state       done
-      tracked			=letter
+      state       retrieving
+      tracked        =letter
 )
 
+;; Information retireval productions
+;; Not tested
+
+(P remmeber-situation
+    =goal>
+     ISA    goal
+     state     retrieving
+    =retireval>
+     ISA    estimation
+     action =act
+    ?manual>
+     state free
+   ==>
+    =goal>
+      state nil
+    +manual>
+     cmd press-key
+     key =act
+     
+    @retrieval>)
+
+(P cant-remember-up
+    =goal>
+      ISA goal
+      state retrieving
+    = y (+ pos-y dy)
+    <= dy 0
+    ?retrieval>
+      buffer failure
+    ?manual>
+      state free
+   ==>
+     =goal>
+       state nil
+     +manual>
+       cmd press-key
+       key "u")
+
+(P cant-remember-down
+    =goal>
+      ISA goal
+      state retrieving
+    = y (+ pos-y dy)
+    > dy 0
+    ?retrieval>
+      buffer failure
+    ?manual>
+      state free
+   ==>
+     =goal>
+       state nil
+     +manual>
+       cmd press-key
+       key "d")
+
+(P cant-remember-stay
+    =goal>
+      ISA goal
+      state retrieving
+      - y  (+ pos-y dy)
+    ?retrieval>
+      buffer failure
+    ?manual>
+      state free
+   ==>
+     =goal>
+       state nil
+     +manual>
+       cmd press-key
+       key "s")    
 
 (set-all-base-levels 100000 -1000)
 (goal-focus goal)
