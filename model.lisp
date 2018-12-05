39c78ca7ae12362be36d635ea929c0205cd06123
Nathan Painchaud <nathanpainchaud@outlook.com>
2018-12-05 17:25:02 -0500

    Improved environment perception

--- model.lisp
+++ model.lisp
@@ -6,121 +6,199 @@
 
 (define-model jameson
 
+;; Model parameters
 (sgp :v t :esc nil :lf 0.4 :bll 0.5 :ans 0.5 :rt 0 :ncnar nil :show-focus t :trace-detail low)
 
-  
-;; PRODUCTIONS - let's try to keep them organized
-;; GENERAL
+;; Chunk types definitions
+(chunk-type position pos-x pos-y)
+(chunk-type environment jameson-position oldest-position middle-position newest-position
+state jameson-encoded projectile-encoded)
 
-(chunk-type position posx posy)
-(chunk-type trajectory position1 position2 position3 course state encode-state)
-(chunk-type projectile id trajectory)
-
+;; Declarative memory initialization
 (add-dm
-  (goal isa trajectory)(attended)(attending)(done)(pos-encoded)
-  (pos1-encoded)(pos2-encoded)(pos3-encoded)(estimation))
+  (goal isa environment state start)
+	(start)
+	(attending) (encoding-jameson) (encoding-projectile) (take-action)
+	(estimating) (done))
 
 
+;; Environment perception productions
 
-;; VISUAL-MODULE PRODUCTION
-
-(P attend-projectile
-   =goal>
-      ISA         trajectory
-			state				nil
-   =visual-location>
-   		screen-x				=pos-x
-   		screen-y				=pos-y
-   ?visual>
-      state       free
-   ?imaginal>
-   		state				free
-   
-==>
-   +visual>
-      cmd         move-attention
-      screen-pos  =visual-location
-   =goal>
-      state       nil
-   +imaginal>
-   		isa					position
-   		posx				=pos-x
-   		posy				=pos-y
-)
-
-
-(P encode-trajectory-one
+; Detects an object in the environment
+; and moves the attention of Jameson to the object
+(P attend-object
 		=goal>
-			isa						trajectory
-			encode-state	nil
+			isa         			environment
+			state							start
+		=visual-location>
+			screen-x					=x
+			screen-y					=y
+		?visual>
+			state       			free
 		?imaginal>
-			buffer				full
-		=imaginal>
-		?manual>
-			state					free
+			state							free
 ==>
 		=goal>
-			position1			=imaginal
-			encode-state	pos1-encoded
-   	+manual>
-  		cmd       	  press-key
-  		key       	  "s"
-  	+imaginal>
-  	
-)
-(P encode-trajectory-two
-		=goal>
-			isa						trajectory
-			encode-state	pos1-encoded
-		?imaginal>
-			buffer				full
-		=imaginal>
-		?manual>
-			state					free
-==>
-		=goal>
-			position2			=imaginal
-			encode-state	pos2-encoded
+			state       			attending
+		+visual>
+			cmd         			move-attention
+			screen-pos  			=visual-location
 		+imaginal>
-   	+manual>
-  		cmd       	  press-key
-  		key       	  "s"
-)
-(P encode-trajectory-three
-		=goal>
-			isa						trajectory
-			encode-state	pos2-encoded
-		?imaginal>
-			buffer 				full
-		=imaginal>
-		?manual>
-			state 				free
-==>
-		=goal>
-			position3		  =imaginal
-			encode-state  estimation
-   	+manual>
-  		cmd    	     press-key
-  		key    	     "s"
-  	+imaginal>
-  	
-)
-(P estimate-course
-		=goal>
-			isa						trajectory
-			encode-state	estimation		
-			position1			=p1
-==>
-		!output!				"Estimating trajectory"
-		=goal>
-			encode-state	estimate
-		+retrieval>
-			isa						position
-			pos-xy				=p1
-)
+			isa								position
+			pos-x							=x
+			pos-y							=y)
 
-			
-			
+; Encodes the position where Jameson perceives himself
+(P encode-jameson
+		=goal>
+			isa								environment
+			state							attending
+		=visual>
+		  value							"J"
+		?imaginal>
+			buffer						full
+==>
+		=goal>
+			state							encoding-jameson
+		+visual-location>
+      :attended    			nil)
+
+; Encodes the position of an attended projectile
+(P encode-projectile
+		=goal>
+			isa								environment
+			state							attending
+		=visual>
+		- value							"J"
+		?imaginal>
+			buffer						full
+==>
+		=goal>
+			state							encoding-projectile
+		+visual-location>
+      :attended    			nil)
+
+; Adds the position where Jameson perceives himself at
+(P add-position-jameson
+		=goal>
+			isa								environment
+			state							encoding-jameson
+		?imaginal>
+			buffer						full
+		=imaginal>
+==>
+		=goal>
+			state							start
+			jameson-position	=imaginal
+			jameson-encoded		done)
+
+; Adds a position to the projectile's trajectory when the
+; positions for the previous 2 or 3 timesteps are known
+(P add-position-full-trajectory
+		=goal>
+			isa								environment
+			state							encoding-projectile
+			middle-position		=middle-position
+			newest-position		=newest-position
+		?imaginal>
+			buffer						full
+		=imaginal>
+==>
+		=goal>
+			state							start
+			projectile-encoded	done
+			oldest-position		=middle-position
+			middle-position		=newest-position
+			newest-position		=imaginal)
+
+; Adds a position to the projectile's trajectory when the
+; position for previous timestep is known
+(P add-position-partial-trajectory
+		=goal>
+			isa								environment
+			state							encoding-projectile
+			middle-position		nil
+			newest-position		=newest-position
+		?imaginal>
+			buffer						full
+		=imaginal>
+==>
+		=goal>
+			state							start
+			projectile-encoded	done
+			middle-position		=newest-position
+			newest-position		=imaginal)
+
+; Adds a position to the projectile's trajectory when no
+; previous positions are known
+(P add-position-no-trajectory
+		=goal>
+			isa								environment
+			state							encoding-projectile
+			newest-position		nil
+		?imaginal>
+			buffer						full
+		=imaginal>
+==>
+		=goal>
+			state							start
+			projectile-encoded	done
+			newest-position		=imaginal)
+
+; Stops the model from progressing if it does not have
+; enough information to guess the behavior of the projectile
+(P cant-take-action
+		=goal>
+			isa         			environment
+			state							start
+			jameson-encoded		done
+			projectile-encoded	done
+		  oldest-position		nil
+		?manual>
+			state							free
+==>
+		=goal>
+			jameson-encoded		nil
+			projectile-encoded	nil
+		+manual>
+  		cmd								press-key
+  		key								"s"
+		+imaginal>)
+
+; Allows the model to progress if it has enough information
+; to guess the behavior of the projectile
+(P can-take-action
+		=goal>
+			isa         			environment
+			state							start
+			jameson-encoded		done
+			projectile-encoded	done
+		- oldest-position		nil
+==>
+		=goal>
+			jameson-encoded		nil
+			projectile-encoded	nil
+			state							estimating
+		+imaginal>)
+
+
+;; TODO Trajectory estimation productions
+(P match-trajectory
+		=goal>
+			isa         			environment
+			state							estimating
+		?manual>
+			state							free
+==>
+		!output!						"Estimating trajectory"
+		=goal>
+			state							nil
+			jameson-position	nil
+		+manual>
+  		cmd								press-key
+  		key								"s")
+
+;; TODO Move decision productions
 
 (set-all-base-levels 100000 -1000)
 (goal-focus goal)
