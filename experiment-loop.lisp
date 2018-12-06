b582f5d2984191490365aa9ec5db5e309c2eb91a
Nathan Painchaud <nathanpainchaud@outlook.com>
2018-12-06 15:00:43 -0500

    Clarified calls to ACT-R model from the Lisp program

--- experiment-loop.lisp
+++ experiment-loop.lisp
@@ -20,9 +20,7 @@
 
     (proc-display :clear t)
 
-    (setf *start-time* (get-time *run-model*))
-
-    window))
+    (setf *start-time* (get-time *run-model*))))
 
 ; Function that calls the model with the results of the trial to allow the model to learn from them
 (defun show-model-results (results)
@@ -40,10 +38,12 @@
               (append *results*
                       (list (make-result :hit (is-jameson-hit *jameson* (timestep-projectiles timestep))
                                          :time *trial-time*))))
+        (show-model-results (first (last *results*)))
         (setf *trial-time* 0)
-        (show-model-results (first (last *results*))))
-      (setf *trial-time* (+ *trial-time* timestep-time)))
-    (move-jameson-on-decision *jameson* (string key))
+        (setf *jameson* (make-instance 'jameson)))
+      (progn
+        (setf *trial-time* (+ *trial-time* timestep-time))
+        (move-jameson-on-decision *jameson* (string key))))
     (when *timesteps*
       (present-timestep (first *timesteps*)))))
 
@@ -51,7 +51,5 @@
 (defun collect-responses (timestep-count visible)
   (setf *results* nil)
   (setf *trial-time* 0)
-  (let ((window (present-timestep (first *timesteps*) :new-window t :visible visible)))
-    (run (* *max-response-time* timestep-count) :real-time visible)
-    (while (null *results*)
-      (allow-event-manager window))))
+  (present-timestep (first *timesteps*) :new-window t :visible visible)
+  (run (* *max-response-time* timestep-count) :real-time visible))
